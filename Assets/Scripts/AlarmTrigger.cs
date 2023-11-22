c3a335cbf4258a07c7e1f3cb98168bc1100d7519
Vladislav <GimliVlad@list.ru>
2023-11-22 23:00:09 +0300

    change name

--- Assets/Scripts/AlarmTrigger.cs
+++ Assets/Scripts/AlarmTrigger.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using DG.Tweening;
 
-public class Alarm : MonoBehaviour
+public class AlarmTrigger : MonoBehaviour
 {
     [SerializeField] private UnityEvent<bool> _alarmed;
 
